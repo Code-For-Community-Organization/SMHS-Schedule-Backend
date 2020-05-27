cf62d2090785b15560205b257e9959645837e650
Jevon Mao <jevonmao@JevondeiMac.attlocal.net>
2020-05-26 20:16:45 -0700

    Parsing missing assignments and all into JSON

--- main_module/main.py
+++ main_module/main.py
@@ -1,15 +1,18 @@
 import json
+from bs4 import BeautifulSoup
 
 
 with open ('data.txt', 'r') as json_file:
     file = json.load(json_file)
 
 
-# aaa = """Period: {period}, Room: {room}, Teacher: {teacher}, Grade: {grade}, 
-#           Last updated: {update}""".format(period = file[0]['Period'], 
-#           room = file[0]["RoomNumber"], teacher = file[0]["TeacherName"],
-#           grade = file[0]["CurrentMarkAndScore"], update=file[0]['LastUpdated']
 
-parsed_json = [{k:v for k,v in classes.items() if k in ['Period', 'RoomNumber', 'TeacherName', 'CurrentMarkandScore', 'LastUpdated']} for classes in file]
+parsed_json = [{k:v for k,v in classes.items() if k in ['Period', 'RoomNumber', 'TeacherName', 'Percent', 'Average', 'CurrentMark', 'MissingAssignments', 'LastUpdated', 'SchoolName','DistrictName']} for classes in file]
+
+
+for classes in parsed_json:
+    soup = BeautifulSoup(classes["MissingAssignments"],"html.parser")
+    misAssignNum = soup.get_text()
+    classes["MissingAssignments"] = misAssignNum
 
 print(parsed_json)
