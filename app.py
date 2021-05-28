ae4e9b0f2f93304ce44b3e88123d435fd7963933
Jevon Mao <woodburyjevonmao@gmail.com>
2021-05-28 13:15:24 -0700

    Add printing log messages

--- app.py
+++ app.py
@@ -11,15 +11,21 @@ def home():
     if 'email' in request.args and 'password' in request.args:
         email: str = request.args['email']
         password: str = request.args['password']
+        print("Received email: {0}, password: {1}".format(email, password))
         try:
             requestData: Request = Request("https://aeries.smhs.org/Parent/LoginParent.aspx?page=Dashboard.aspx",
                                             password,
                                             email)
+            print("requestData created, ready to load summary")
             rawJson = requestData.loadSummary()
+            print("Raw json fetched: {0}".format(rawJson))
             dataParser: DataParser = DataParser(rawJson)
             parsedPeriods: list[Period] = dataParser.parseData()
+            print("Parsed periods: {0}".format(parsedPeriods))
             encodedPeriods: str = PeriodEncoder().encode(parsedPeriods)
+            print("Encoded periods: {0}".format(encodedPeriods))
         except JSONDecodeError:
+            print("JSONDecoderError, abort 404")
             abort(404)
         return encodedPeriods
     else:
