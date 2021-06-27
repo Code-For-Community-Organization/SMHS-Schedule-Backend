a2cd2750d1b010458923230a9941e0548b0c3da2
Jevon Mao <woodburyjevonmao@gmail.com>
2021-06-27 10:00:32 -0700

    Begin scraping daily annoucements

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from json.decoder import JSONDecodeError
-from flask import Flask, render_template, url_for, abort, Response, request
-import json
-import os
-from urllib import parse
+from flask import Flask, request
 from src.scraper import EmptyPasswordError, FailedAttemptsError, Request, DataParser, Period, PeriodEncoder, ValidationError
 
 app = Flask(__name__)
