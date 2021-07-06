d27ba7e2d5b64ebbb3b83c3ed62d27f381293fd0
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-06 11:47:07 -0700

    Implement JSON database and CRUD operation methods to store user data (#2)
    * Begin implementing JSON database and encrypt data
    
    * Implement save and read operation for JSON db
    
    * Implement error and exception handling
    
    * Implement delete user method
    
    * Refactor DatabseManager CRUD methods into files
    
    * Save to database on api get request
    
    * Remove <pre> tag from response
    
    * Enforce type checking
    
    * Added grades to user database and tests
    
    * Resolve import path issues
    
    * Rollback period object decoder to previous

--- app.py
+++ app.py
@@ -1,41 +1,75 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from sources.AeriesScraper import Request, DataParser, Period, PeriodEncoder, ValidationError
-from typing import List
-    
+from sources.DatabaseManager import DatabaseManager
+from sources.User import User
+from typing import List, Optional
+from cryptography.fernet import InvalidToken
+
+
 def wrapTojsonHTML(content: str, appendBraces: bool = True) -> str:
-        preTag: str = '<pre style="word-wrap: break-word; white-space: pre-wrap;">'
-        return f"{preTag}{{ {content} }}</pre>" if appendBraces else f"{preTag}{content}</pre>"
+  return f"<pre>{{ {content} }}</pre>" if appendBraces else f"<pre>{content}</pre>"
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+
 @app.route('/', methods=['GET'])
 def home():
-    return "Please go to /api/v1/grades/ for RESTful API."
+    return redirect(url_for("API"), 302)
+
+
 @app.route('/api/v1/grades/', methods=['GET'])
 def API():
+    # Verify that URL query parameter includes
+    # email and password we need
     if 'email' in request.args and 'password' in request.args:
+
+        # Get email and password as their string
         email: str = request.args['email']
         password: str = request.args['password']
+
+        # Check email and password not empty
         if email and password:
             try:
+                # Initialize networking request
                 requestData: Request = Request(password, email)
+                # Login to Aeries
                 requestData.login()
-                rawJson = requestData.fetchSummary()
-                dataParser: DataParser = DataParser(rawJson)
-                parsedPeriods: List[Period] = dataParser.parseData()
-                encodedPeriods: str = PeriodEncoder().encode(parsedPeriods)
-                return wrapTojsonHTML(encodedPeriods, appendBraces=False)
+                rawJson: Optional[str] = requestData.fetchSummary()
+                if rawJson is not None:
+                    dataParser: DataParser = DataParser(rawJson)
+                    parsedPeriods: List[Period] = dataParser.parseData()
+                    try:
+                        manager = DatabaseManager()
+                        manager.newUserEntry(user=User(email=email, password=password, grades=parsedPeriods))
+                    except ValueError as err:
+                        errorMessage: str = f"Internal: {err}"
+                        print(errorMessage)
+                    except TypeError as err:
+                        errorMessage: str = f"Internal: {err}"
+                        print(errorMessage)
+                    except InvalidToken as err:
+                        errorMessage: str = f"Internal: {err}"
+                        print(errorMessage)
+                    encodedPeriods: str = PeriodEncoder().encode(parsedPeriods)
+                    return encodedPeriods
+                    
+                else:
+                    errorMessage: str = """Internal: Server encountered error when
+                     fetching summary after login. Please file a bug report."""
+                    return errorMessage, 500
 
             except ValidationError as err:
-                return wrapTojsonHTML(str(err))
+                return str(err), 401
         else:
             errorMessage: str = "Error: Email and password cannot be empty."
-            return wrapTojsonHTML(errorMessage), 400
+            return errorMessage, 400
     else:
-        errorMessage: str = "Error: No email and password provided. Please provide a valid email and password login."
-        return wrapTojsonHTML(errorMessage), 403
+        errorMessage: str = """Error: No email and password provided. 
+        Please provide a valid email and password login."""
+        return errorMessage, 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ sources/DatabaseManager.py
@@ -0,0 +1,216 @@
+from cryptography.fernet import Fernet, InvalidToken
+from typing import Optional, Dict, List, Any
+import os
+import json
+from json import JSONDecodeError
+if __name__ == "__main__":
+    from AeriesScraper import Period, PeriodEncoder
+    from User import User
+else:
+    from .User import User
+    from .AeriesScraper import Period, PeriodEncoder
+
+debug = True
+if 'ON_HEROKU' in os.environ:
+    debug = False
+
+# typealias
+# Describes an array of dictionary, each containing
+# a key of type string, and a value of either type
+# string, or a dictionary with key and value of type string
+jsonDB = List[Dict[str, Any]]
+
+
+class DatabaseManager:
+    def __init__(self):
+        if debug:
+            self.databaseName = 'debug-database.json'
+            key = b'udmMAqGywVOeu1JXjAt3jc2UsjgoVhoGXBPXbR1ALYE='
+        else:
+            self.databaseName = 'production-database.json'
+            serverKey: Optional[str] = os.environ.get('CRYPTO_KEY')
+            if serverKey is not None:
+                key: bytes = str.encode(serverKey)
+            else:
+                raise NameError(
+                    "Environment variable for crypto key is not found on server.")
+
+        self.cipherSuite = Fernet(key)
+
+    # ---------------- Helper Methods ----------------
+    def _containsDuplicates(self, dbJSON: jsonDB, email: str) -> bool:
+        doesContain = False
+        for user in dbJSON:
+            try:
+                decodedEmail = self._decodeCipher(user['email'])
+                if decodedEmail is not None:
+                    if decodedEmail == email:
+                        doesContain = True
+                else:
+                    raise TypeError(
+                        "While decoding a email in database, it returned a None value.")
+            except InvalidToken as err:
+                print(err)
+        return doesContain
+
+    def _isValid(self, user: User) -> bool:
+        if user.email == '' or user.password == '':
+            return False
+        else:
+            return True
+
+    # ---------------- CRUD Operation Methods ----------------
+    # Instance method
+    Successful = bool
+
+    def deleteUserEntry(self, email: str) -> Successful:
+        if email == '':
+            return False
+        # Open DB in read mode
+        with open(self.databaseName, "r+") as db:
+            # Load JSON
+            dbJSON: jsonDB = json.load(db)
+            try:
+                # Get list of indexes of matching email predicate
+                userIndexes = [i for (i, n) in enumerate(
+                    dbJSON) if self._decodeCipher(n['email']) == email]
+
+                # We only expect 1 item in list, get 1st index
+                if userIndexes:
+                    userIndex = userIndexes[0]
+                else:
+                    return False
+                # Remove User at that index
+                dbJSON.pop(userIndex)
+                db.truncate(0)  # Wipe file content
+                db.seek(0)  # Move file pointer to beginning
+                json.dump(dbJSON, db)
+                return True
+            # Handle error where crypto token invalid
+            except InvalidToken as err:
+                print(err)
+
+            # Handle error where index cannot be found in list
+            except ValueError as err:
+                print(err)
+            return False
+
+    # Instance method - Create new user
+    def newUserEntry(self, user: User):
+        if not self._isValid(user):
+            raise ValueError("Empty username or password is not allowed.")
+        with open(self.databaseName, "a+") as db:
+            db.seek(0)
+            cipherEmail = self._encodeCipher(data=user.email)
+            cipherPassword = self._encodeCipher(data=user.password)
+            if cipherEmail is not None and cipherPassword is not None:
+                newUser = {'email': cipherEmail,
+                           'password': cipherPassword,
+                           'grades': self._newUserGradesEntry(periods=user.grades)}
+                try:
+                    # DB already exists
+                    dbJSON: jsonDB = json.load(db)
+
+                    # Append new user if its email does not already exist
+                    if not self._containsDuplicates(dbJSON, email=user.email):
+                        dbJSON.append(newUser)
+
+                    db.truncate(0)  # Wipe file content
+                    db.seek(0)  # Move file pointer to beginning
+                    json.dump(dbJSON, db)
+
+                except JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+                    # DB does not exist, create new
+                    dbJSON: jsonDB = [newUser]
+                    json.dump(dbJSON, db)
+            else:
+                raise TypeError(
+                    "While encoding email and/or password, it returned a None value.")
+    
+    #Instance method - Help construct user's grades data
+    def _newUserGradesEntry(self, periods: List[Period]) -> List[Dict[str, Any]]:
+        periodGrades: List[Dict[str, Any]] = []
+        for period in periods:
+            jsonFormat = PeriodEncoder().encode(period)
+            dictFormat = json.loads(jsonFormat)
+            periodGrades.append(dictFormat)
+        return periodGrades
+
+    # Instance method
+    def getUserEntry(self, email: str) -> Optional[User]:
+        if email == '':
+            return None
+        # Open DB in read mode
+        with open(self.databaseName, "r") as db:
+            # Load JSON
+            dbJSON = json.load(db)
+            try:
+                # Filter for matching email
+                matchingUser = filter(lambda u: self._decodeCipher(
+                    u['email']) == email, dbJSON)
+                firstMatch: Optional[Dict[str, Any]] = next(matchingUser)
+
+                if firstMatch is not None:
+                    # Decrypt information
+                    decryptedEmail = self._decodeCipher(firstMatch['email'])
+                    decryptedPassword = self._decodeCipher(
+                        firstMatch['password'])
+                    if decryptedPassword is not None and decryptedEmail is not None:
+                        # Construct user
+                        targetUser = User(email=decryptedEmail,
+                                          password=decryptedPassword,
+                                          grades=firstMatch['grades'])
+                        # return user
+                        return targetUser
+                    else:
+                        raise TypeError(
+                            "While decoding email and/or password, it returned a None value.")
+                else:
+                    return None
+            except InvalidToken as err:
+                print(err)
+
+# ---------------- Encoding and Decoding Data ----------------
+    def _encodeCipher(self, data: str) -> Optional[str]:
+        try:
+            encrypted = self.cipherSuite.encrypt(str.encode(data))
+            cipherString = encrypted.decode("utf-8")
+            return cipherString
+        except TypeError:
+            return None
+
+    def _decodeCipher(self, data: str) -> Optional[str]:
+        try:
+            decrypted = self.cipherSuite.decrypt(str.encode(data))
+            cipherString = decrypted.decode("utf-8")
+            return cipherString
+        except TypeError:
+            return None
+
+
+if __name__ == "__main__":
+    databaseManager1 = DatabaseManager()
+    databaseManager2 = DatabaseManager()
+    try:
+        testPeriod1 = Period(periodNum=1,
+                            periodName="English",
+                            teacherName="Donald",
+                            gradePercent=98.3,
+                            currentMark="A",
+                            isPrior=False)
+        testPeriod2 = Period(periodNum=2,
+                            periodName="Algebra 1",
+                            teacherName="Biden",
+                            gradePercent=93.5,
+                            currentMark="A-",
+                            isPrior=False)
+        newUser = User(email="fdsf@smhst.org", password="%", grades=[testPeriod1, testPeriod2])
+        newUser2 = User(email="s", password="1", grades=[testPeriod2, testPeriod1])
+        databaseManager1.newUserEntry(user=newUser)
+        databaseManager2.newUserEntry(user=newUser2)
+        print(databaseManager1.getUserEntry(email="s"))
+        print(databaseManager2.getUserEntry(email="s"))
+
+    except Exception as e:
+        print(e)
