2b8fecfb5d2b2f71b4cf5f33677781a64daa9eca
Jevon Mao <jingwenmao@g.ucla.edu>
2021-07-06 15:11:26 -0700

    Further implement scheduleAsyncFetch

--- sources/AsyncNetwork.py
+++ sources/AsyncNetwork.py
@@ -1,24 +1,32 @@
+from sources.User import Student
 import aiohttp
 import asyncio
+from typing import List
+
 
 async def asyncNetworkGet(loginURL: str,
                           summaryURL: str,
-                          username: str,
+                          email: str,
                           password: str):
-    
+
     async with aiohttp.ClientSession() as session:
         payload: dict[str, str] = {
-        'portalAccountUsername': username,
-        'portalAccountPassword': password}
+            'portalAccountUsername': email,
+            'portalAccountPassword': password}
         async with session.post(loginURL, data=payload) as response:
             async with session.get(summaryURL) as response:
-                print(f"Now fetching {username}. Status:", response.status)
+                print(f"Now fetching {email}. Status:", response.status)
                 html = await response.text()
                 print(f"{html[:10]}...")
 
-async def main():
-    await asyncio.gather(*[asyncNetworkGet(loginURL='https://aeries.smhs.org/Parent/LoginParent.aspx',
-                    summaryURL='https://aeries.smhs.org/Parent/Widgets/ClassSummary/GetClassSummary?IsProfile=True',
-                    username='jingwen.mao@smhsstudents.org',
-                    password='Mao511969') for i in range(1)])
-asyncio.run(main())
+
+async def scheduleAsyncFetch(students: List[Student]):
+    loginURL = 'https://aeries.smhs.org/Parent/LoginParent.aspx'
+    summaryURL = 'https://aeries.smhs.org/Parent/Widgets/ClassSummary/GetClassSummary?IsProfile=True'
+
+    await asyncio.gather(*[asyncNetworkGet(loginURL,
+                                           summaryURL,
+                                           email=s.email,
+                                           password=s.password) for s in students])
+    await asyncio.sleep(5)
+
